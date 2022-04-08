583b81bef1bfd457cb6036c9bf7cdd6da80005ee
Whattyu <71334131+whattyu@users.noreply.github.com>
2022-04-08 21:05:44 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My Name Is Jake</p>
+        <p>My Name Is Whattyu 🙋</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (Welcome to the my Blog Site! 🎉)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
